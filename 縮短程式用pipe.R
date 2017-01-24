88344ba1360715026dbcfb26393e2576c0eb2ba4
B05208038 <B05208038@ntu.edu.tw>
2017-01-24 17:39:04 +0800

    01242017 OwenYeh

---
+++ 縮短程式用pipe.R
@@ -0,0 +1,13 @@
+load(url("https://storage.googleapis.com/r_rookies/straw_hat_df.RData"))
+this_year <- as.numeric(format(Sys.Date(), '%Y'))
+birth_year <- this_year - straw_hat_df$age
+birth_date_chr <- paste(birth_year, straw_hat_df$birthday, sep = "-")
+straw_hat_df$birth_date <- as.Date(birth_date_chr)
+straw_hat_df$birth_date
+#interpret---------------------------------------------------------------------
+straw_hat_df$birth_date  <- Sys.Date() %>%
+  format(format = "%Y") %>%
+  as.numeric() %>%
+  '-' (straw_hat_df$age) %>%
+  paste(straw_hat_df$birthday, sep = "-") 
+straw_hat_df$birth_date  
